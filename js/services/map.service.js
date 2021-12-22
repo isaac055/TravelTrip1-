6cde21320b6f834811781b944d96f16a8e90a2ad
DoronBarshap <doron.prog@gmail.com>
2021-12-22 16:48:59 +0200

    added google maps key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyCoKuRZKzh1SZICQhvXxX-6B9l1t_sR038'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
